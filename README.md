82d5c32a1ea829ac0063eaa8d477870e8037f864
Fabio <fab.ramohitaj@gmail.com>
2022-06-29 16:19:07 +0200

    update

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 * Costruire una classe coppia che deve offrire dei metodi per gestire una coppia di oggetti del medesimo tipo:   :question:
 	* Metodi per poter ottenere/cambiare il primo/secondo elemento della coppia.
 	* Un costruttore che riceve come parametri due riferimenti ad oggetti del medesimo tipo.
-		* Guardare in [src/com/example/coppia](https://github.com/FabioR1995/ProgrammazioneOrientataAgliOggetti/tree/main/src/com/example/coppia) per vedere come è stata realizzata la classe coppia :white_check_mark:		
+		* Guardare in [src/com/example/coppia](https://github.com/FabioR1995/ProgrammazioneOrientataAgliOggetti/tree/main/src/com/example/coppia) per vedere come è stata realizzata la classe coppia :white_check_mark:	
+* 
--- test/com/test/coppia/CoppiaSenzaGenericsTest.java
+++ test/com/test/coppia/CoppiaSenzaGenericsTest.java
@@ -21,5 +21,29 @@ public class CoppiaSenzaGenericsTest {
 		Persona persona = (Persona)coppia.getPrimo();  //downcast da Object a Persona per poter utilizzare i metodi del primo oggetto persona
 		assertSame(pippo,persona.getNome()); // verifica che abbiano lo stesso riferimento
 	}
+	
+	@Test
+	public void testCheNonCompila() {
+		Coppia coppia = new Coppia();
+		String pippo = new String("pippo");
+		String pluto = new String("pluto");
+		Persona p1 = new Persona(pippo);
+		coppia.setPrimo(p1);
+		Persona p2 = new Persona(pluto);
+		coppia.setSecondo(p2);
+		assertSame(pippo,coppia.getPrimo().getNome());  //visto che il tipo statico è object chiaramente non possiamo utilizzare il metodo getNome()		
+	}
+	
+	@Test
+	public void testCheCompilaMaNonEsegue() {
+		Coppia coppia = new Coppia();
+		String pippo = new String("pippo");
+		String pluto = new String("pluto");
+		Persona p1 = new Persona(pippo);
+		coppia.setPrimo(pippo);                 //guardate cosa è stato messo qui come primo oggetto 
+		Persona p2 = new Persona(pluto);
+		coppia.setSecondo(p2);
+		assertSame(pippo,((Persona)coppia.getPrimo()).getNome());  //classCastException a tempo di esecuzione, dovuto al fatto che stiamo castando una Stringa in Persona
+	}
 
 }
