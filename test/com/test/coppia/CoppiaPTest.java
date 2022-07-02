84a1b8282ad8e68c7740f429957a77bfcc602905
Fabio <fab.ramohitaj@gmail.com>
2022-07-02 13:57:50 +0200

    update

---
+++ test/com/test/coppia/CoppiaPTest.java
@@ -0,0 +1,23 @@
+package com.test.coppia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.coppia.CoppiaP;
+import com.example.persona.Persona;
+
+public class CoppiaPTest {
+
+	@Test
+	public void test() {
+		CoppiaP<Persona> coppia;
+		Persona p1 = new Persona("pippo");
+		Persona p2 = new Persona("Pluto");
+		coppia = new CoppiaP<Persona>(p1,p2);
+		assertSame(p1,coppia.getPrimo());
+		assertSame(p2,coppia.getSecondo());
+	}
+
+}
