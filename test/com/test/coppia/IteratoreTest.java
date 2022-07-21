dca02c22bc411241305233a2b5df9baf88a09b94
Fabio <fab.ramohitaj@gmail.com>
2022-07-21 22:31:43 +0200

    update

---
+++ test/com/test/coppia/IteratoreTest.java
@@ -0,0 +1,51 @@
+package com.test.coppia;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IteratoreTest {
+	private List<String> vuota;
+	private List<String> singoletto;
+	private String solitario;
+
+	@Before
+	public void setUp() throws Exception {
+		this.vuota = new ArrayList<String>();
+		this.singoletto = new ArrayList<String>();
+		this.solitario = new String("solitario");
+		this.singoletto.add(solitario); 
+	}
+
+	@Test
+	public void testHasNext_ListaVuota() {
+		Iterator<String>  it = this.vuota.iterator();
+		assertNotNull(it);
+		assertFalse(it.hasNext());
+	}
+	
+	@Test
+	public void testHasNext_PrimaSiPoiNoSuSingoletto() {       
+		Iterator<String> it = this.singoletto.iterator();
+		assertNotNull(it);
+		assertTrue(it.hasNext());
+		it.next();
+		assertFalse(it.hasNext());
+	}
+	
+	@Test
+	public void testNext_singoletto() {
+		Iterator<String> it = this.singoletto.iterator();
+		assertNotNull(it);
+		assertTrue(it.hasNext());
+		assertSame(this.solitario, it.next());
+		System.out.println(this.solitario);
+		System.out.println(it);
+	}
+
+}
